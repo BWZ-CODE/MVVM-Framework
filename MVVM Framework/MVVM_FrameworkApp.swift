f2826111c03d143a90c1d5e4da63ea1a0fa393ff
BWZ-CODE <mmige24@gmail.com>
2025-09-22 20:12:38 -0500

    Initial Commit

---
+++ MVVM Framework/MVVM_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_FrameworkApp.swift
+//  MVVM Framework
+//
+//  Created by Miguel Angel Bohorquez on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
